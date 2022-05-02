9a0143aac851a780c8dec430d6c4eef87009d384
Chris <kimballc3@yahoo.com>
2022-05-01 23:28:51 -0500

    readmes are happening

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,95 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require('fs')
+const renderTemplate = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//These questions should help the user create a professional readme. 
 
-// TODO: Create a function to initialize app
-function init() {}
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-// Function call to initialize app
-init();
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+
+
+const questions = () => {
+  return inquirer.prompt([ 
+    {
+      type: "input",
+      message: "What is the project title?",
+      name: "title"
+    },
+
+    {
+      type: "input",
+      message: "What is the project description?",
+      name: "description"
+    },
+
+    {
+      type: "input",
+      message: "What are the installation requirements?",
+      name: "install"
+    },
+
+    {
+      type: "input",
+      message: "What are the usage instructions? (How can someone else use this?)",
+      name: "usage"
+    },
+
+    {
+      type: "input",
+      message: "Who contributed to this project?",
+      name: "contribution"
+    },
+
+    {
+      type: "input",
+      message: "What are the requirements for testing this project?",
+      name: "test"
+    },
+
+    {
+      type: "input",
+      message: "Which license would you like to add? (MIT, BSD, or GNU. If you are not using a license, press ENTER to skip this question)",
+      name: "license",
+    },
+
+    { type: "input", 
+    message: "What is your GitHub account name?", 
+    name: "github"
+    },
+
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email"
+    },
+  ]);
+};
+
+
+questions()
+ .then((answers) => {
+    const template = renderTemplate(answers);
+
+  fs.writeFile('README.md', template, err => {
+    if (err) throw new Error(err);
+    console.log('Your Readme.md file is ready. Check it out!')
+  })
+});
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,81 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns the license section of README
+const renderLicenseSection = (license) => {
+  if (!license) {
+    return '';
+  }
+  return `## License
+  This project uses the ${license} license.`
+}
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) => {
+  if (!license) {
+    return '';
+  } 
+  if (license === 'MIT') return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  if (license === 'BSD') return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+  if (license === 'GNU') return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseLink = (license) => {
+//  return `[License Information](https://opensource.org/licenses/${license})`
+if (!license) {
+  return '';
+} 
+if (license === 'MIT') return 'https://opensource.org/licenses/MIT'
+if (license === 'BSD') return 'https://opensource.org/licenses/BSD-3-Clause'
+if (license === 'GNU') return 'https://www.gnu.org/licenses/gpl-3.0.en.html'
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `${renderLicenseBadge(data.license)}
+# ${data.title} 
+## Description
 
+${data.description}
+
+## Table of Contents
+   
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Badges](#badges)
+- [Contribute](#contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.install}
+
+## Usage Instructions
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
+${renderLicenseBadge(data.license)}
+
+## Contributions
+
+${data.contribution}
+
+## Test Instructions/How to test this Project
+
+${data.test}
+
+## Questions
+Questions? Please email me at ${data.email}.
+Do you like this project and want to see what else I'm doing? Here is my Github: [${data.github}](https://github.com/${data.github}).
 `;
 }
 
-module.exports = generateMarkdown;
+
+
+// module.exports = {renderTemplate, renderLicenseSection, renderLicenseLink, renderLicenseBadge}
+// module.exports = {generateMarkdown, renderTemplate, renderLicenseSection, renderLicenseLink, renderLicenseBadge}
+module.exports = generateMarkdown
